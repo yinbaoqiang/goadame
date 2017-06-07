6d3321a3a3bea2a0e4c11abd4960e5199e2bd8cd
ybq <yinbaoqiang@antlinker.com>
2017-06-07 18:21:10 +0800

    增加新的定义

---
+++ design/admin.go
@@ -0,0 +1,47 @@
+package design
+
+import (
+	"github.com/goadesign/goa/design" // Use . imports to enable the DSL
+	"github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = apidsl.Resource("regevent", func() { // Resources group related API endpoints
+	apidsl.BasePath("/admin/event") // together. They map to REST resources for REST
+
+	apidsl.Action("add", func() { // Actions define a single API endpoint together
+		apidsl.Description("注册事件监听")        // with its path, parameters (both path
+		apidsl.Routing(apidsl.POST("/reg")) // parameters and querystring values) and payload
+
+		apidsl.Payload(func() { // (shape of the request body).
+			apidsl.Member("etype", design.String, "事件类型")
+			apidsl.Member("action", design.String, "事件行为,不设置该项则注册监听所有行为变化")
+			apidsl.Member("from", design.String, "产生事件的服务器标识")
+			apidsl.Member("bakurl", design.String, "回调路径")
+			apidsl.Required("etype")
+			apidsl.Required("bakurl")
+			apidsl.Required("from")
+
+		})
+		apidsl.Response(design.OK, RegResultMedia)
+	})
+	apidsl.Action("remove", func() { // Actions define a single API endpoint together
+		apidsl.Description("取消事件监听") // with its path, parameters (both path
+
+		apidsl.Routing(apidsl.DELETE("/:rid")) // parameters and querystring values) and payload
+		apidsl.Params(func() {                 // (shape of the request body).
+			apidsl.Param("rid", design.String, "事件监听唯一标识")
+
+		})
+
+		apidsl.Response(design.OK, CreResultMedia)
+	})
+	apidsl.Action("list", func() { // Actions define a single API endpoint together
+		apidsl.Description("注册事件监听")   // with its path, parameters (both path
+		apidsl.Routing(apidsl.GET("")) // parameters and querystring values) and payload
+		apidsl.Params(func() {
+			apidsl.Param("page", design.Integer, "事件类型")
+			apidsl.Param("count", design.Integer, "事件行为,不设置该项则注册监听所有行为变化")
+		})
+		apidsl.Response(design.OK, apidsl.ArrayOf(RegListMedia))
+	})
+})
--- design/media.go
+++ design/media.go
@@ -28,3 +28,56 @@ var ErrMedia = apidsl.MediaType("application/vnd.ant.error+json", func() {
 		apidsl.Attribute("msg")
 	})
 })
+
+// CreResultMedia defines the media type used to render bottles.
+var RegResultMedia = apidsl.MediaType("application/vnd.ant.result+json", func() {
+	apidsl.Description("注册事件监听成功")
+	apidsl.Attributes(func() { // Attributes define the media type shape.
+		apidsl.Attribute("ok", design.Boolean, "成功标识")
+
+	})
+	apidsl.View("default", func() { // View defines a rendering of the media type.
+		apidsl.Attribute("ok") // Media types may have multiple views and must
+	})
+})
+
+// RegInfoMedia 注册监听事件列表
+var RegInfoMedia = apidsl.MediaType("application/vnd.ant.reg+json", func() {
+	apidsl.Description("事件监听信息")
+
+	apidsl.Attributes(func() { // (shape of the request body).
+		apidsl.Attribute("rid", design.String, "注册事件监听唯一标识")
+		apidsl.Attribute("etype", design.String, "事件类型")
+		apidsl.Attribute("action", design.String, "事件行为,不设置该项则注册监听所有行为变化")
+		apidsl.Attribute("from", design.String, "产生事件的服务器标识")
+		apidsl.Attribute("bakurl", design.String, "回调路径")
+		apidsl.Required("rid")
+		apidsl.Required("etype")
+		apidsl.Required("bakurl")
+		apidsl.Required("from")
+
+	})
+	apidsl.View("default", func() { // View defines a rendering of the media type.
+		apidsl.Attribute("ridok")  // Media types may have multiple views and must
+		apidsl.Attribute("etype")  // Media types may have multiple views and must
+		apidsl.Attribute("action") // Media types may have multiple views and must
+		apidsl.Attribute("from")   // Media types may have multiple views and must
+		apidsl.Attribute("bakurl") // Media types may have multiple views and must
+	})
+})
+
+// RegListMedia 注册监听事件列表
+var RegListMedia = apidsl.MediaType("application/vnd.ant.reg.list+json", func() {
+	apidsl.Description("事件监听列表")
+
+	apidsl.Attributes(func() { // (shape of the request body).
+		apidsl.Attribute("total", design.Integer, "总数量")
+		apidsl.Attribute("list", apidsl.ArrayOf(RegInfoMedia), "事件类型")
+		apidsl.Required("total")
+
+	})
+	apidsl.View("default", func() { // View defines a rendering of the media type.
+		apidsl.Attribute("total") // Media types may have multiple views and must
+		apidsl.Attribute("list")  // Media types may have multiple views and must
+	})
+})
